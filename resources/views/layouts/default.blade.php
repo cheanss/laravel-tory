ce8c2607f8a1273123fae16ccdafa11da32344f6
Jack.Shi <jack.shi@keerle.net>
2016-12-30 17:26:14 +0800

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,10 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="/css/app.css">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
 </head>
 <body>
-    @yield('content')
+    @include('layouts._header')
+    <div class="container">
+        @yield('content')
+        @include('layouts._footer')
+    </div>
 </body>
 </html>
