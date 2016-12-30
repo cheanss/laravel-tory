c2fadb80cf75c9d1036e4eeb6771a57dc291ffae
Jack.Shi <jack.shi@keerle.net>
2016-12-30 16:24:14 +0800

    Finish static pages

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title', '关于我们')
+@section('content')
+    <h3>关于页面</h3>
+@stop
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title', '帮助')
+@section('content')
+    <h2>帮主页面</h2>
+@stop
