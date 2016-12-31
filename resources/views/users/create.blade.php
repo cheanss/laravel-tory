fa374081ff9c3050fc682c5a3685ee315c4d5f11
Jack.Shi <jack.shi@keerle.net>
2016-12-31 15:10:54 +0800

    Finish user signup

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -2,5 +2,34 @@
 @section('title', '注册')
 
 @section('content')
-    <h3>注册</h3>
+    <div class="col-md-offset-2 col-md-8">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h5>注册</h5>
+            </div>
+            <div class="panel-body">
+                @include('shared.errors')
+                <form method="post" action="{{route('users.store')}}">
+                    {{csrf_field()}}
+                    <div class="form-group">
+                        <label for="name">姓名：</label>
+                        <input type="text" name="name" class="form-control" id="name" value="{{old('name')}}">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">邮箱：</label>
+                        <input type="text" name="email" class="form-control" id="email" value="{{old('email')}}">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">密码：</label>
+                        <input type="password" name="password" class="form-control" id="password" value="{{old('password')}}">
+                    </div>
+                    <div class="form-group">
+                        <label for="password_confirmation">确认密码：</label>
+                        <input type="password" name="password_confirmation" class="form-control" id="password_confirmation" value="{{old('password_confirmation')}}">
+                    </div>
+                    <button type="submit" class="btn btn-primary">注册</button>
+                </form>
+            </div>
+        </div>
+    </div>
 @stop
