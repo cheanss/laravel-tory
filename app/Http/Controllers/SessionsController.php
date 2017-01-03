5c8cc2bae224097bd1f61ac76427bd34fba6b8a7
Jack.Shi <jack.shi@keerle.net>
2017-01-03 15:38:09 +0800

    Finish login and logout

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Auth;
+
+class SessionsController extends Controller
+{
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email|max:255',
+            'password' => 'required',
+        ]);
+
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
+
+        // 该用户存在于数据库，且邮箱和密码相符合
+        if(Auth::attempt($credentials, $request->has('remember'))) {
+            // 登录成功后的操作
+            session()->flash('success', '欢迎回来！');
+            return redirect()->route('users.show', [Auth::user()]);
+        } else {
+            // 登录失败后的操作
+            session()->flash('danger', '很抱歉，您的邮箱或密码不正确');
+            return redirect()->back();
+        }
+    }
+
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '您已成功退出！');
+        return redirect('login');
+    }
+}
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+use Auth;
+
 class UsersController extends Controller
 {
     public function create()
@@ -23,19 +25,25 @@ class UsersController extends Controller
 
     public function store(Request $request)
     {
+        // 验证用户提交数据
         $this->validate($request, [
             'name' => 'required|max:50',
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed',
         ]);
 
+        // 创建用户并写入数据库
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => $request->password,
         ]);
 
+        // 用户注册成功后自动登录
+        Auth::login($user);
+        // 提示语，flash 第一个参数和 bootstrap 的类进行结合提示成功样式语句
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        // 跳转到用户详情页
         return redirect()->route('users.show', [$user]);
     }
 }
